7a620504a5007543fc0356a01df9945b84dbbaf6
Shivkumar <shiv.rv007@gmail.com>
2024-10-09 18:33:27 +0530

    CICD-Pipeline 6

--- update_website.sh
+++ update_website.sh
@@ -4,15 +4,19 @@ REPO_URL="https://github.com/007kumar/my-website.git"
 REPO_DIR="/path/to/local/repo"
 WEBSITE_DIR="/var/www/html/"
 # These lines set up some information about where your files are
+
 # Pull latest changes
 cd $REPO_DIR || exit
 git pull origin main
 # This part goes to your local copy of the website and gets the latest changes
+
 # Copy files to website directory
 rsync -av --delete $REPO_DIR/ $WEBSITE_DIR/
 # This copies the new files to where Nginx can find them
+
 # Restart Nginx
 sudo systemctl restart nginx
 # This restarts Nginx to make sure it sees the new files
+
 echo "Website updated successfully"
 # This prints a message saying the update is done
